d2ce5fcc36be030a641657114aedc68fb554ecf3
LeO-GuRdU <leogurdulich@gmail.com>
2021-05-03 11:02:49 -0300

    Agrego el resto de los archivos

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+    define('DB_NAME','Family');
+    define('DB_USER','root');
+    define('DB_PASSWORD','');
+    define('DB_HOST','localhost');
+    $conn = @mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+    if (!$conn){
+        die('Could not Connect: '.mysqli_connect_error());
+    }
+?>
